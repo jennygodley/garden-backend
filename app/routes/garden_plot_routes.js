94770a76c0fd066d3a15ee64425fd8d842703180
jennygodley <jennifergodley@gmail.com>
2019-10-15 11:30:46 -0400

    Removed dead code
    Added some extra code in testing that's been deleted.

--- app/routes/garden_plot_routes.js
+++ app/routes/garden_plot_routes.js
@@ -101,30 +101,6 @@ router.patch('/gardenPlots/:id', requireToken, removeBlanks, (req, res, next) =>
     .catch(next)
 })
 
-// UPDATE
-// PATCH /gardenPlots/5a7db6c74d55bc51bdf39793/plant
-router.patch('/gardenPlots/:id/plant', requireToken, removeBlanks, (req, res, next) => {
-  // if the client attempts to change the `owner` property by including a new
-  // owner, prevent that by deleting that key/value pair
-  delete req.body.gardenPlot.owner
-
-  gardenPlot.findById(req.params.id)
-    .then(handle404)
-    .then(gardenPlot => {
-      console.log(req.body.plant)
-      // pass the `req` object and the Mongoose record to `requireOwnership`
-      // it will throw an error if the current user isn't the owner
-      requireOwnership(req, gardenPlot)
-
-      // pass the result of Mongoose's `.update` to the next `.then`
-      return gardenPlot.updateOne(req.body.gardenPlot)
-    })
-    // if that succeeded, return 204 and no JSON
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
-
 // DESTROY
 // DELETE /gardenPlots/5a7db6c74d55bc51bdf39793
 router.delete('/gardenPlots/:id', requireToken, (req, res, next) => {
